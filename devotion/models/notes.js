8c8aa11099fcaf456aac4ffeeba1d563f2895e6c
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-02-19 06:43:20 -0800

    db

---
+++ devotion/models/notes.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new.mongoose.Schema({
+	title: { 
+		type: String, 
+		required: true ,
+		unique: true,
+		maxlength: [40, "Title cannot be more than 40 characters"]
+	},
+	description: {
+		type: String,
+		required: true,
+		maxlength: [200, "Description cannot be more than 200 characters"]
+	}
+
+})
+
+module.exports = Mongoose.models.Note || mongoose.models('Note', NoteSchema);
--- devotion/next.config.js
+++ devotion/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGO_URI="mongodb+srv://xenofy23:Senpou_23@cluster0.rx3rj.mongodb.net/devotions?retryWrites=true&w=majority"
+  }
 }
 
 module.exports = nextConfig
