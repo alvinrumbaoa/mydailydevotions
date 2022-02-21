4bda48650423fac6fdc932f280620146debd34d1
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-02-20 19:02:25 -0800

    test

--- devotion/next.config.js
+++ devotion/next.config.js
@@ -2,5 +2,11 @@ module.exports = {
   reactStrictMode: true,
   env:{
     mongoUri:  "mongodb+srv://xenofy23:Senpou_23@cluster0.rx3rj.mongodb.net/devotions?retryWrites=true&w=majority"
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  
 }
