ef77ea5262127d48bfbf8103ab6fd7d8957fc8af
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2022-03-15 22:47:39 -0700

    commit

--- devotion/pages/api/notes/index.js
+++ devotion/pages/api/notes/index.js
@@ -12,16 +12,20 @@ export default async (req, res) => {
 			const notes = await Note.find({})
 
 			res.status(200).json({success: true, data:notes})
+			console.log(res.json())
 		}catch (error) {
 			res.status(400).json({success: false})
+			console.log(res.json())
 		}
 		case 'POST':
 		try{
 			const note = await Note.create(req.body)
 
-			res.status(201).json({success: true, data:note })
+			res.status(201).json({success: true, data: note })
+			console.log(res.json())
 		}catch (error) {
 			res. status(400).json({success: false})
+			console.log(res.json())
 		}
 		break;
 		default:
--- devotion/pages/create.js
+++ devotion/pages/create.js
@@ -3,16 +3,39 @@ import {Flex, Heading, Input, FormControl, Textarea, Button} from '@chakra-ui/re
 import {useRouter} from 'next/router'
 const create = () => {
 	const [verse, setVerse] = useState("");
-	const 
+	const [message, setMessage] = useState("");
+	const [application, setApplication] = useState("");
+	const [errors, setErrors] = useState({})
+	const router = useRouter();
 
 
-	return (
-		<Flex h={'100vh'} background='teal.100'>
-			<Flex>
 
-			</Flex>
-		</Flex>
-	)
+	const handleSubmission = async () => {
+		const info = {
+		  verse: "Kieran",
+		  message: "message",
+		  application: "application is the best"
+		}
+	
+		const res = await fetch("/api/notes", {
+		  method: "POST",
+		  headers: {
+			"Content-Type": "application/json"
+		  },
+		  body: JSON.stringify(info)
+		});
+	
+		const data = await res.json();
+		console.log(data) // { message: "success" }
+	  }
+	
+	  return (
+		<div>
+		  <button onClick={handleSubmission}>
+			Submit
+		  </button>
+		</div>
+	  )
 }
 
 export default create
